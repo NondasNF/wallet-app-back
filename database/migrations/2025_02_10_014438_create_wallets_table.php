64c7c2a8d108ad8905d8d3bcf708de1c92611981
epaminondas <nondasnoronha@gmail.com>
2025-02-10 01:52:44 -0300

    Add WalletFactory to improve seeds

--- database/migrations/2025_02_10_014438_create_wallets_table.php
+++ database/migrations/2025_02_10_014438_create_wallets_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->decimal('balance', 10, 2)->default(0);
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
